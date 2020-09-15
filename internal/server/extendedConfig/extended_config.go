2d08f622c4197e7899b1e236500fdf78c514a4e2
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2020-09-15 14:24:36 +0530

    Added: extended config which consists of prometheus config along with the config coming in with CLI command

---
+++ internal/server/extendedConfig/extended_config.go
@@ -0,0 +1,30 @@
+package extendedConfig
+
+import (
+	"github.com/aquasecurity/trivy/internal/server/config"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type ExtendedConfig struct {
+	Config config.Config
+	MetricsRegistry *prometheus.Registry
+	GaugeMetric     *prometheus.GaugeVec
+}
+
+func New(c config.Config) ExtendedConfig {
+	return ExtendedConfig{
+		Config: c,
+	}
+}
+
+func (ec *ExtendedConfig) Init() {
+	ec.MetricsRegistry = prometheus.NewRegistry()
+	ec.GaugeMetric = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "trivy",
+			Help: "Gauge Metrics associated with trivy - Last DB Update, Last DB Update Attempt ...",
+		},
+		[]string{"action"},
+	)
+	ec.MetricsRegistry.MustRegister(ec.GaugeMetric)
+}
